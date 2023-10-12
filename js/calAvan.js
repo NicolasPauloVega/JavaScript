1138728ea7d2944f8cab62d512b02fb093d5478a
Nick08020608 <145580278+NicolasPauloVega@users.noreply.github.com>
2023-10-12 10:24:10 -0500

    A1

---
+++ js/calAvan.js
@@ -0,0 +1,59 @@
+function sumar(n1,n2){
+    var resultado = n1+n2;
+    var res = document.getElementById("resultado")
+
+    if(n1!=0 && n2!=0){
+        respuesta.style.display="block";
+        res.innerHTML = `Total: ${resultado}`;
+        res.style.color = "green";
+    }else{
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
+    }
+}
+
+function resta(n1,n2){
+    var resultado = n1-n2;
+    var resultado = document.getElementById("resultado")
+
+    if(n1!=0 && n2!=0){
+        respuesta.style.display="block";
+        res.innerHTML = `Total: ${resultado}`;
+        res.style.color = "green";
+    }else{
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
+    }
+}
+
+function multiplicacion(n1,n2){
+    var resultado = n1*n2;
+    var resultado = document.getElementById("resultado")
+
+    if(n1!=0 && n2!=0){
+        respuesta.style.display="block";
+        res.innerHTML = `Total: ${resultado}`;
+        res.style.color = "green";
+    }else{
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
+    }
+}
+
+function division(n1,n2){
+    var resultado = n1/n2;
+    var resultado = document.getElementById("resultado")
+
+    if(n1!=0 && n2!=0){
+        respuesta.style.display="block";
+        res.innerHTML = `Total: ${resultado}`;
+        res.style.color = "green";
+    }else{
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
+    }
+}
