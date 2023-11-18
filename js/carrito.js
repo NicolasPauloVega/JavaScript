065db9763c0c2f06a386522a65c39b73db7c59d8
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-18 16:24:54 -0500

    carrito

--- js/carrito.js
+++ js/carrito.js
@@ -17,3 +17,29 @@ function decremento(){
     }
 }
 
+function resetear(){
+    contador = 0;
+    valor.innerHTML = contador;
+}
+
+const valor1 = document.getElementById("valor1")
+
+function aumento(){
+    contador += 1;
+    valor1.innerHTML = contador;
+}
+
+function desaumento(){
+    if(contador > 0){
+        contador -= 1;
+        valor1.innerHTML = contador;
+    }else{
+        contador = 0;
+        valor1.innerHTML = contador;
+    }
+}
+
+function resetear1(){
+    contador = 0;
+    valor1.innerHTML = contador;
+}
