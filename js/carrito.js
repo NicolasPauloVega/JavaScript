8dfa6d2ee205637347cd57df5592fbd7a83efed1
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-27 17:44:00 -0500

    A9

--- js/carrito.js
+++ js/carrito.js
@@ -1,90 +1,104 @@
-let contadorHtml = 0;
-let contadorCss = 0;
-let contadorJs = 0;
+//Creamos 3 variables que pueden cambiar de valor
+let contadorHtml = 0; //Para la cantidad del libros html
+let contadorCss = 0; //Para la cantidad del libro css
+let contadorJs = 0; //Para la cantidad del libro javascript
 
-const valor = document.getElementById('valor');
-const valor1 = document.getElementById('valor1');
-const valor2 = document.getElementById('valor2');
+//Se crean 3 constante que no permitan cambiar su valor
+const valor = document.getElementById('valor'); //atrae el valor de la cantidad del libro html
+const valor1 = document.getElementById('valor1'); //atrae el valor de la cantidad del libro css
+const valor2 = document.getElementById('valor2'); //atrae el valor de la cantidad del libro javascript
+const total = document.getElementById('resultado'); //Atrae los valores que tiene el total del pedido
 
-const total = document.getElementById('resultado');
-
+//Se crea una funcion la cual sumará la cantidad de clics que haga el usuario en el boton más (+).
 function incrementoHtml(){
     contadorHtml += 1;
-    valor.innerHTML = contadorHtml
+    valor.innerHTML = contadorHtml //Se envia al contador de html
 }
 
+//Se crea una funcion la cual restará la cantidad de clics que haga el usuario en el boton menos (-).
 function decrementoHtml(){
+    //Se usa una condicion la cual identificará si el numero es mayor este mismo restará 1 al valor que se tenga si el numero es igual a 0 no se realizará la resta.
     if(contadorHtml > 0){
         contadorHtml -= 1;
-        valor.innerHTML = contadorHtml;
+        valor.innerHTML = contadorHtml; //Se envia al contador de html
     }else{
         contadorHtml = 0;
-        valor.innerHTML = contadorHtml;
+        valor.innerHTML = contadorHtml; //Se envia al contador de html
     }
 }
 
+//Se crea una funcion la cual reiniciará todo el conteo de la cantidad al momento de presionar el boton de reinicio (reset)
 function resetearHtml(){
     contadorHtml = 0;
-    valor.innerHTML = contadorHtml;
+    valor.innerHTML = contadorHtml; //Se envia al contador de html
 }
 
-
+//Se crea una funcion la cual sumará la cantidad de clics que haga el usuario en el boton más (+).
 function incrementCss(){
     contadorCss += 1;
-    valor1.innerHTML = contadorCss;
+    valor1.innerHTML = contadorCss; //Se envia al contador de css
 }
 
+//Se crea una funcion la cual restará la cantidad de clics que haga el usuario en el boton menos (-).
 function decrementCss(){
-    contadorCss -= 1;
-    valor1.innerHTML = contadorCss;
-}
-
-function resetCss(){
+    //Se usa una condicion la cual identificará si el numero es mayor este mismo restará 1 al valor que se tenga si el numero es igual a 0 no se realizará la resta.
     if(contadorCss > 0){
         contadorCss -= 1;
-        valor1.innerHTML = contadorCss;
+        valor1.innerHTML = contadorCss; //Se envia al contador de css
     }else{
         contadorCss = 0;
-        valor1.innerHTML = contadorCss;
+        valor1.innerHTML = contadorCss; //Se envia al contador de css
     }
 }
 
+//Se crea una funcion la cual reiniciará todo el conteo de la cantidad al momento de presionar el boton de reinicio (reset)
+function resetCss(){
+    contadorCss = 0;
+    valor1.innerHTML = contadorCss; //Se envia al contador de css
+}
+
+//Se crea una funcion la cual sumará la cantidad de clics que haga el usuario en el boton más (+).
 function incrementoJs(){
     contadorJs += 1;
-    valor2.innerHTML = contadorJs;
+    valor2.innerHTML = contadorJs; //Se envia al contador de javascript
 }
 
+//Se crea una funcion la cual restará la cantidad de clics que haga el usuario en el boton menos (-).
 function decrementoJs(){
+    //Se usa una condicion la cual identificará si el numero es mayor este mismo restará 1 al valor que se tenga si el numero es igual a 0 no se realizará la resta.
     if(contadorJs > 0){
         contadorJs -= 1;
-        valor2.innerHTML = contadorJs;
+        valor2.innerHTML = contadorJs; //Se envia al contador de javascript
     }else{
         contadorJs = 0;
-        valor2.innerHTML = contadorJs;
+        valor2.innerHTML = contadorJs; //Se envia al contador de javascript
     }
 }
 
+//Se crea una funcion la cual reiniciará todo el conteo de la cantidad al momento de presionar el boton de reinicio (reset)
 function resetearJs(){
     contadorJs = 0;
-    valor2.innerHTML = contadorJs;
+    valor2.innerHTML = contadorJs; //Se envia al contador de javascript
 }
 
+//Se crea una funcion la cual calcule el total de todo el pedido.
 function calcular(){
 
-    const html = parseFloat(document.getElementById("html").value);
-    const valorH = contadorJs;
+    //Se crean constantes que almacenen los valores predeterminados de una lista desplegable y otra que almacene el conteo actual de cada uno de los libros.
+    const html = parseFloat(document.getElementById("html").value); //Almacena valores de la lista html.
+    const valorH = contadorHtml; //Almacena los valores del contador de html
 
-    const css = parseFloat(document.getElementById("css").value);
-    const valorC = contadorCss;
+    const css = parseFloat(document.getElementById("css").value); //Almacena valores de la lista css
+    const valorC = contadorCss; //Almacena valores del contador css
 
-    const javascript = parseFloat(document.getElementById("javascript").value);
-    const valorJ = contadorJs;
+    const javascript = parseFloat(document.getElementById("javascript").value); //Almacena valores de la lista javascript
+    const valorJ = contadorJs; //Almacena valores del contador javascript
 
-    var resHtml = html * valorH;
-    var resCss = css * valorC;
-    var resJs = javascript * valorJ;
+    var resHtml = html * valorH; //Se calcula el valor total de html
+    var resCss = css * valorC; //Se calcula el valor total de css
+    var resJs = javascript * valorJ; //Se calcula el valor total de javascript
 
-    var totalP = resHtml + resCss + resJs;
+    var totalP = resHtml + resCss + resJs; //Se calcula el valor total del pedido
 
-    total.innerHTML = totalP;
+    total.innerHTML = totalP; //se muestra el resultado total del pedido
 }
